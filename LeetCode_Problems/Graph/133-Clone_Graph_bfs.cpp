ad6f83335d2c3b173d0639ce6e16c32c95b66500
Jeremy Pai <jeremypai1128@gmail.com>
2024-07-14 16:55:01 +0800

    update solution of 133. Clone Graph with bfs method

---
+++ LeetCode_Problems/Graph/133-Clone_Graph_bfs.cpp
@@ -0,0 +1,59 @@
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+// Definition for a Node.
+class Node {
+public:
+  int val;
+  std::vector<Node *> neighbors;
+  Node() {
+    val = 0;
+    neighbors = std::vector<Node *>();
+  }
+  Node(int _val) {
+    val = _val;
+    neighbors = std::vector<Node *>();
+  }
+  Node(int _val, std::vector<Node *> _neighbors) {
+    val = _val;
+    neighbors = _neighbors;
+  }
+};
+
+class Solution {
+public:
+  Node *cloneGraph(Node *node) {
+    if (node == nullptr) {
+      return nullptr;
+    }
+
+    if (node->neighbors.empty()) {
+      return new Node(node->val);
+    }
+
+    std::unordered_map<Node *, Node *> hashMap;
+    hashMap[node] = new Node(node->val);
+    std::queue<Node *> q;
+    q.push(node);
+    while (true) {
+      if (q.empty()) {
+        break;
+      }
+
+      Node *cur = q.front();
+      for (const auto neighbor : cur->neighbors) {
+        const auto found = hashMap.find(neighbor);
+        if (found == hashMap.end()) {
+          hashMap[neighbor] = new Node(neighbor->val);
+          q.push(neighbor);
+        }
+        hashMap[cur]->neighbors.emplace_back(hashMap[neighbor]);
+      }
+
+      q.pop();
+    }
+
+    return hashMap[node];
+  }
+};
