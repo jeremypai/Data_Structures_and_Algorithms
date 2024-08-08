a7b3e578954dc4819d24d02cfc02602b059e5ea8
Jeremy Pai <jeremypai1128@gmail.com>
2024-08-09 00:33:20 +0800

    update solution of 21. Merge Two Sorted Lists
    - update simpler solution

--- LeetCode_Problems/Linked_List/21-Merge_Two_Sorted_Lists.cpp
+++ LeetCode_Problems/Linked_List/21-Merge_Two_Sorted_Lists.cpp
@@ -1,55 +1,30 @@
 // Definition for singly-linked list.
 struct ListNode {
   int val;
-  ListNode *next;
+  ListNode* next;
   ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
 class Solution {
  public:
-  ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
-    if (list1 == nullptr && list2 == nullptr) {
-      return nullptr;
-    }
-
-    if (list1 == nullptr) {
-      return list2;
-    }
-
-    if (list2 == nullptr) {
-      return list1;
-    }
-
-    ListNode *dummyHead = new ListNode(0);
-    ListNode *cur = dummyHead;
-    while (true) {
-      if (list1 == nullptr && list2 == nullptr) {
-        break;
-      }
-
-      if (list1 != nullptr && list2 != nullptr) {
-        if (list1->val >= list2->val) {
-          cur->next = list2;
-          cur = cur->next;
-          list2 = list2->next;
-        } else {
-          cur->next = list1;
-          cur = cur->next;
-          list1 = list1->next;
-        }
-      } else if (list1 != nullptr) {
-        cur->next = list1;
-        cur = cur->next;
-        list1 = list1->next;
-      } else /* list2 != nullptr */ {
+  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* ans = new ListNode();
+    ListNode* cur = ans;
+    while (list1 && list2) {
+      if (list1->val >= list2->val) {
         cur->next = list2;
-        cur = cur->next;
         list2 = list2->next;
+      } else {
+        cur->next = list1;
+        list1 = list1->next;
       }
+      cur = cur->next;
     }
 
-    return dummyHead->next;
+    cur->next = list1 ? list1 : list2;
+
+    return ans->next;
   }
 };
