4ac3a1e4b67fe0f34126205cc059b050019556d0
Jeremy Pai <jeremypai1128@gmail.com>
2024-08-17 20:15:16 +0800

    update simpler solution of 98. Validate Binary Search Tree

--- LeetCode_Problems/Tree/98-Validate_Binary_Search_Tree.cpp
+++ LeetCode_Problems/Tree/98-Validate_Binary_Search_Tree.cpp
@@ -1,66 +1,35 @@
 // Definition for a binary tree node.
 struct TreeNode {
   int val;
-  TreeNode *left;
-  TreeNode *right;
+  TreeNode* left;
+  TreeNode* right;
   TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right)
+  TreeNode(int x, TreeNode* left, TreeNode* right)
       : val(x), left(left), right(right) {}
 };
 
 class Solution {
  public:
-  bool isValidBST(TreeNode *root) { return isValidBSTInternal(root); }
+  bool isValidBST(TreeNode* root) {
+    return isValidBSTRec(root, nullptr, nullptr);
+  }
 
  private:
-  bool isValidBSTInternal(TreeNode *node) {
+  bool isValidBSTRec(TreeNode* node, int* lower, int* upper) {
     if (node == nullptr) {
       return true;
     }
 
-    TreeNode *leftTreeMaxNode = getMaxNode(node->left);
-    if (leftTreeMaxNode != nullptr && leftTreeMaxNode->val >= node->val) {
+    if (lower && node->val <= *lower) {
       return false;
     }
 
-    TreeNode *rightTreeMinNode = getMinNode(node->right);
-    if (rightTreeMinNode != nullptr && rightTreeMinNode->val <= node->val) {
+    if (upper && node->val >= *upper) {
       return false;
     }
 
-    return isValidBSTInternal(node->left) && isValidBSTInternal(node->right);
-  }
-
-  TreeNode *getMaxNode(TreeNode *node) {
-    if (node == nullptr) {
-      return nullptr;
-    }
-
-    while (true) {
-      if (node->right == nullptr) {
-        break;
-      }
-
-      node = node->right;
-    }
-
-    return node;
-  }
-
-  TreeNode *getMinNode(TreeNode *node) {
-    if (node == nullptr) {
-      return nullptr;
-    }
-
-    while (true) {
-      if (node->left == nullptr) {
-        break;
-      }
-
-      node = node->left;
-    }
-
-    return node;
+    return isValidBSTRec(node->left, lower, &node->val) &&
+           isValidBSTRec(node->right, &node->val, upper);
   }
 };
