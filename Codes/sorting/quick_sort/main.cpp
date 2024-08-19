33a823e8699d9f87f951c3b81e47d0703e624d58
Jeremy Pai <jeremypai1128@gmail.com>
2024-08-20 01:38:34 +0800

    update quick sort algorithm and use 912. Sort an Array for testing algorithm

--- Codes/sorting/quick_sort/main.cpp
+++ Codes/sorting/quick_sort/main.cpp
@@ -1,13 +1,14 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include "quick_sort.h"
 
 TEST(QuickSortTest, SimpleUnsortedList) {
-  int array[] = {5, 2, 9, 1, 5, 6};
-  int sortedArray[] = {1, 2, 5, 5, 6, 9};
-  int size = sizeof(array) / sizeof(array[0]);
-  quickSort(array, 0, size - 1);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {5, 2, 9, 1, 5, 6};
+  std::vector<int> sortedArray = {1, 2, 5, 5, 6, 9};
+  quickSort(array, 0, array.size() - 1);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -15,11 +16,10 @@ TEST(QuickSortTest, SimpleUnsortedList) {
 }
 
 TEST(QuickSortTest, ReverseSortedList) {
-  int array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-  int sortedArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int size = sizeof(array) / sizeof(array[0]);
-  quickSort(array, 0, size - 1);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+  std::vector<int> sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  quickSort(array, 0, array.size() - 1);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -27,11 +27,10 @@ TEST(QuickSortTest, ReverseSortedList) {
 }
 
 TEST(QuickSortTest, SortedList) {
-  int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int sortedArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int size = sizeof(array) / sizeof(array[0]);
-  quickSort(array, 0, size - 1);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<int> sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  quickSort(array, 0, array.size() - 1);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -39,11 +38,10 @@ TEST(QuickSortTest, SortedList) {
 }
 
 TEST(QuickSortTest, ListWithRepeatedElements) {
-  int array[] = {3, 6, 2, 8, 2, 6, 3, 8};
-  int sortedArray[] = {2, 2, 3, 3, 6, 6, 8, 8};
-  int size = sizeof(array) / sizeof(array[0]);
-  quickSort(array, 0, size - 1);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {3, 6, 2, 8, 2, 6, 3, 8};
+  std::vector<int> sortedArray = {2, 2, 3, 3, 6, 6, 8, 8};
+  quickSort(array, 0, array.size() - 1);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -51,11 +49,10 @@ TEST(QuickSortTest, ListWithRepeatedElements) {
 }
 
 TEST(QuickSortTest, SingleElementList) {
-  int array[] = {1};
-  int sortedArray[] = {1};
-  int size = sizeof(array) / sizeof(array[0]);
-  quickSort(array, 0, size - 1);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {1};
+  std::vector<int> sortedArray = {1};
+  quickSort(array, 0, array.size() - 1);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -63,11 +60,10 @@ TEST(QuickSortTest, SingleElementList) {
 }
 
 TEST(QuickSortTest, ListWithNegativeNumbers) {
-  int array[] = {-3, -1, -7, -4, -5, -2};
-  int sortedArray[] = {-7, -5, -4, -3, -2, -1};
-  int size = sizeof(array) / sizeof(array[0]);
-  quickSort(array, 0, size - 1);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {-3, -1, -7, -4, -5, -2};
+  std::vector<int> sortedArray = {-7, -5, -4, -3, -2, -1};
+  quickSort(array, 0, array.size() - 1);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
--- Codes/sorting/quick_sort/quick_sort.h
+++ Codes/sorting/quick_sort/quick_sort.h
@@ -1,6 +1,37 @@
 #ifndef QUICK_SORT_H_
 #define QUICK_SORT_H_
 
-void quickSort(int array[], int low, int high);
+#include <cstdlib>
+#include <vector>
+
+int partition(std::vector<int>& array, int left, int right);
+
+inline void quickSort(std::vector<int>& array, int left, int right) {
+  if (left >= right) {
+    return;
+  }
+
+  int pivot = partition(array, left, right);
+  quickSort(array, left, pivot - 1);
+  quickSort(array, pivot + 1, right);
+}
+
+inline int partition(std::vector<int>& array, int left, int right) {
+  // randomized choose pivot position
+  int pivotIndex = rand() % (right - left + 1);
+  std::swap(array[left + pivotIndex], array[right]);
+  int pivot = array[right];
+
+  int i = left;
+  for (int j = left; j < right; ++j) {
+    if (array[j] < pivot) {
+      std::swap(array[i], array[j]);
+      ++i;
+    }
+  }
+
+  std::swap(array[i], array[right]);
+  return i;
+}
 
 #endif  // ! QUICK_SORT_H_
