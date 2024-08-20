5aee07655a54f15341f07d14c39570716171d5e2
Jeremy Pai <jeremypai1128@gmail.com>
2024-08-21 00:06:58 +0800

    update insertion sort solution
    - use vector and std::swap directly for simpler solution

--- Codes/sorting/insertion_sort/insertion_sort.h
+++ Codes/sorting/insertion_sort/insertion_sort.h
@@ -1,6 +1,19 @@
 #ifndef INSERTION_SORT_H_
 #define INSERTION_SORT_H_
 
-void insertionSort(int array[], int size);
+#include <vector>
+
+inline void insertionSort(std::vector<int>& array) {
+  for (int i = 0; i < array.size(); ++i) {
+    for (int j = i - 1; j >= 0; --j) {
+      if (array[j] > array[j + 1]) {
+        std::swap(array[j], array[j + 1]);
+      } else {
+        // REMARK more efficient than bubble sort
+        break;
+      }
+    }
+  }
+}
 
 #endif  // !INSERTION_SORT_H_
--- Codes/sorting/insertion_sort/main.cpp
+++ Codes/sorting/insertion_sort/main.cpp
@@ -1,13 +1,14 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include "insertion_sort.h"
 
 TEST(InsertionSortTest, SimpleUnsortedList) {
-  int array[] = {5, 2, 9, 1, 5, 6};
-  int sortedArray[] = {1, 2, 5, 5, 6, 9};
-  int size = sizeof(array) / sizeof(array[0]);
-  insertionSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {5, 2, 9, 1, 5, 6};
+  std::vector<int> sortedArray = {1, 2, 5, 5, 6, 9};
+  insertionSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -15,11 +16,10 @@ TEST(InsertionSortTest, SimpleUnsortedList) {
 }
 
 TEST(InsertionSortTest, ReverseSortedList) {
-  int array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-  int sortedArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int size = sizeof(array) / sizeof(array[0]);
-  insertionSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+  std::vector<int> sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  insertionSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -27,11 +27,10 @@ TEST(InsertionSortTest, ReverseSortedList) {
 }
 
 TEST(InsertionSortTest, SortedList) {
-  int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int sortedArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int size = sizeof(array) / sizeof(array[0]);
-  insertionSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<int> sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  insertionSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -39,11 +38,10 @@ TEST(InsertionSortTest, SortedList) {
 }
 
 TEST(InsertionSortTest, ListWithRepeatedElements) {
-  int array[] = {3, 6, 2, 8, 2, 6, 3, 8};
-  int sortedArray[] = {2, 2, 3, 3, 6, 6, 8, 8};
-  int size = sizeof(array) / sizeof(array[0]);
-  insertionSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {3, 6, 2, 8, 2, 6, 3, 8};
+  std::vector<int> sortedArray = {2, 2, 3, 3, 6, 6, 8, 8};
+  insertionSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -51,11 +49,10 @@ TEST(InsertionSortTest, ListWithRepeatedElements) {
 }
 
 TEST(InsertionSortTest, SingleElementList) {
-  int array[] = {1};
-  int sortedArray[] = {1};
-  int size = sizeof(array) / sizeof(array[0]);
-  insertionSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {1};
+  std::vector<int> sortedArray = {1};
+  insertionSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -63,11 +60,10 @@ TEST(InsertionSortTest, SingleElementList) {
 }
 
 TEST(InsertionSortTest, ListWithNegativeNumbers) {
-  int array[] = {-3, -1, -7, -4, -5, -2};
-  int sortedArray[] = {-7, -5, -4, -3, -2, -1};
-  int size = sizeof(array) / sizeof(array[0]);
-  insertionSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {-3, -1, -7, -4, -5, -2};
+  std::vector<int> sortedArray = {-7, -5, -4, -3, -2, -1};
+  insertionSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
