4b99dec1de4d4d90cd84ffb0a32f82d83a126b67
Jeremy Pai <jeremypai1128@gmail.com>
2024-08-21 00:12:58 +0800

    update selection sort algorithm
    - use vector and std::swap directly for simpler solution

--- Codes/sorting/selection_sort/selection_sort.h
+++ Codes/sorting/selection_sort/selection_sort.h
@@ -1,6 +1,18 @@
 #ifndef SELECTION_SORT_H_
 #define SELECTION_SORT_H_
 
-void selectionSort(int array[], int size);
+#include <vector>
+
+inline void selectionSort(std::vector<int>& array) {
+  for (int i = 0; i < array.size(); ++i) {
+    int indexMin = i;
+    for (int j = i + 1; j < array.size(); ++j) {
+      if (array[indexMin] > array[j]) {
+        indexMin = j;
+      }
+    }
+    std::swap(array[indexMin], array[i]);
+  }
+}
 
 #endif  // !SELECTION_SORT_H_
