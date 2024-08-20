f15366a3481e23fbb0cbc3c883e9bf926627061b
Jeremy Pai <jeremypai1128@gmail.com>
2024-08-21 00:00:52 +0800

    update bubble sort algorithm
    - use vector and std::swap directly for simpler solution

--- Codes/sorting/bubble_sort/bubble_sort.h
+++ Codes/sorting/bubble_sort/bubble_sort.h
@@ -1,6 +1,16 @@
 #ifndef BUBBLE_SORT_H_
 #define BUBBLE_SORT_H_
 
-void bubbleSort(int array[], int size);
+#include <vector>
+
+inline void bubbleSort(std::vector<int>& array) {
+  for (int i = 0; i < array.size(); ++i) {
+    for (int j = 0; j < array.size() - 1 - i; ++j) {
+      if (array[j] > array[j + 1]) {
+        std::swap(array[j], array[j + 1]);
+      }
+    }
+  }
+}
 
 #endif  // !BUBBLE_SORT_H_
--- Codes/sorting/bubble_sort/main.cpp
+++ Codes/sorting/bubble_sort/main.cpp
@@ -1,13 +1,14 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include "bubble_sort.h"
 
 TEST(BubbleSortTest, SimpleUnsortedList) {
-  int array[] = {5, 2, 9, 1, 5, 6};
-  int sortedArray[] = {1, 2, 5, 5, 6, 9};
-  int size = sizeof(array) / sizeof(array[0]);
-  bubbleSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {5, 2, 9, 1, 5, 6};
+  std::vector<int> sortedArray = {1, 2, 5, 5, 6, 9};
+  bubbleSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -15,11 +16,10 @@ TEST(BubbleSortTest, SimpleUnsortedList) {
 }
 
 TEST(BubbleSortTest, ReverseSortedList) {
-  int array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-  int sortedArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int size = sizeof(array) / sizeof(array[0]);
-  bubbleSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+  std::vector<int> sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  bubbleSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -27,11 +27,10 @@ TEST(BubbleSortTest, ReverseSortedList) {
 }
 
 TEST(BubbleSortTest, SortedList) {
-  int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int sortedArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int size = sizeof(array) / sizeof(array[0]);
-  bubbleSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<int> sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  bubbleSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -39,11 +38,10 @@ TEST(BubbleSortTest, SortedList) {
 }
 
 TEST(BubbleSortTest, ListWithRepeatedElements) {
-  int array[] = {3, 6, 2, 8, 2, 6, 3, 8};
-  int sortedArray[] = {2, 2, 3, 3, 6, 6, 8, 8};
-  int size = sizeof(array) / sizeof(array[0]);
-  bubbleSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {3, 6, 2, 8, 2, 6, 3, 8};
+  std::vector<int> sortedArray = {2, 2, 3, 3, 6, 6, 8, 8};
+  bubbleSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -51,11 +49,10 @@ TEST(BubbleSortTest, ListWithRepeatedElements) {
 }
 
 TEST(BubbleSortTest, SingleElementList) {
-  int array[] = {1};
-  int sortedArray[] = {1};
-  int size = sizeof(array) / sizeof(array[0]);
-  bubbleSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {1};
+  std::vector<int> sortedArray = {1};
+  bubbleSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
@@ -63,11 +60,10 @@ TEST(BubbleSortTest, SingleElementList) {
 }
 
 TEST(BubbleSortTest, ListWithNegativeNumbers) {
-  int array[] = {-3, -1, -7, -4, -5, -2};
-  int sortedArray[] = {-7, -5, -4, -3, -2, -1};
-  int size = sizeof(array) / sizeof(array[0]);
-  bubbleSort(array, size);
-  for (int i = 0; i < size; ++i) {
+  std::vector<int> array = {-3, -1, -7, -4, -5, -2};
+  std::vector<int> sortedArray = {-7, -5, -4, -3, -2, -1};
+  bubbleSort(array);
+  for (int i = 0; i < array.size(); ++i) {
     ASSERT_EQ(array[i], sortedArray[i])
         << "value is different - array: " << array[i]
         << ", sorted array: " << sortedArray[i];
