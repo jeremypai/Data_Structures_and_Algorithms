55fad674c987383a652bc5da5bc08f7beaea56be
Jeremy Pai <jeremypai1128@gmail.com>
2024-08-22 00:34:17 +0800

    update vector method of 509. Fibonacci Number

--- LeetCode_Problems/Recursion/509-Fibonacci_Number.cpp
+++ LeetCode_Problems/Recursion/509-Fibonacci_Number.cpp
@@ -26,3 +26,21 @@ class SolutionHashMap {
     return hashMap[n];
   }
 };
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class SolutionVector {
+ public:
+  int fib(int n) {
+    if (n <= 1) {
+      return n;
+    }
+    std::vector<int> dp(n + 1, 0);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+  }
+};
