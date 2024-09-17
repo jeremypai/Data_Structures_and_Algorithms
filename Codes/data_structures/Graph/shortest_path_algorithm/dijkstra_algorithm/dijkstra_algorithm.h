8306fe05ef1c080d9b542b2446e3e1cfc1c30389
Jeremy Pai <jeremypai1128@gmail.com>
2024-09-17 18:01:59 +0800

    update Dijkstra class to includ addEdge function and improve the codes of Dijkstra's algorithm

--- Codes/data_structures/Graph/shortest_path_algorithm/dijkstra_algorithm/dijkstra_algorithm.h
+++ Codes/data_structures/Graph/shortest_path_algorithm/dijkstra_algorithm/dijkstra_algorithm.h
@@ -3,57 +3,80 @@
 
 #include <limits>
 #include <queue>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 struct Path {
-  int vertexFrom;
-  int vertexTo;
+  int from;
+  int to;
   int distance;
-  Path(int vertexFrom, int vertexTo, int distance)
-      : vertexFrom(vertexFrom), vertexTo(vertexTo), distance(distance) {}
+  Path(int from, int to, int distance)
+      : from(from), to(to), distance(distance) {}
 };
 
-inline std::vector<int> dijkstra(const std::vector<std::vector<Path>> &graph,
-                                 int start) {
-  std::vector<int> dist(graph.size(), std::numeric_limits<int>::max());
-  std::vector<bool> visited(graph.size(), false);
+class Dijkstra {
+ private:
+  std::unordered_map<int, std::vector<Path>> adjList_;
+  std::unordered_set<int> visited_;
 
-  // priority queue to store vertices to be processed
-  auto compare = [](Path a, Path b) {
-    // cause the smallest distance to appear as the top
-    return a.distance > b.distance;
-  };
-  std::priority_queue<Path, std::vector<Path>, decltype(compare)> pq(compare);
+ public:
+  Dijkstra() = default;
+  ~Dijkstra() = default;
 
-  // add start vertex to priority queue
-  pq.push(Path(start, start, 0));
-
-  while (true) {
-    if (pq.empty()) {
-      break;
+  void addEdge(int u, int v, int w) {
+    // from u to v
+    const auto foundU = adjList_.find(u);
+    if (foundU != adjList_.end()) {
+      foundU->second.emplace_back(Path{u, v, w});
+    } else {
+      adjList_[u] = std::vector<Path>{Path{u, v, w}};
     }
 
-    Path curPath = pq.top();
-    pq.pop();
-
-    if (visited[curPath.vertexTo]) {
-      continue;
-    }
-    visited[curPath.vertexTo] = true;
-    dist[curPath.vertexTo] = curPath.distance;
-
-    for (const auto path : graph[curPath.vertexTo]) {
-      const int vertexFrom = path.vertexFrom;
-      const int vertexTo = path.vertexTo;
-      const int distance = path.distance;
-      int distanceTo = (curPath.distance + distance);
-      if (!visited[vertexTo] && distanceTo < dist[vertexTo]) {
-        pq.push(Path(vertexFrom, vertexTo, distanceTo));
-      }
+    // from v to u
+    const auto foundV = adjList_.find(v);
+    if (foundV != adjList_.end()) {
+      foundV->second.emplace_back(Path{v, u, w});
+    } else {
+      adjList_[v] = std::vector<Path>{Path{v, u, w}};
     }
   }
 
-  return dist;
-}
+  std::vector<int> dijkstra(int start) {
+    // priority queue to store vertices to be processed
+    auto compare = [](Path a, Path b) {
+      // cause the smallest distance to appear as the top
+      return a.distance > b.distance;
+    };
+    std::priority_queue<Path, std::vector<Path>, decltype(compare)> pq(compare);
+
+    // add start vertex to priority queue
+    pq.push(Path{start, start, 0});
+
+    std::vector<int> dist(adjList_.size(), std::numeric_limits<int>::max());
+    while (!pq.empty()) {
+      Path curPath = pq.top();
+      pq.pop();
+
+      if (visited_.count(curPath.to)) {
+        continue;
+      }
+      visited_.insert(curPath.to);
+      dist[curPath.to] = curPath.distance;
+
+      for (const auto& path : adjList_[curPath.to]) {
+        const int& from = path.from;
+        const int& to = path.to;
+        const int& distance = path.distance;
+        int distanceTo = dist[curPath.to] + distance;
+        if (!visited_.count(to) && distanceTo < dist[to]) {
+          pq.push(Path{from, to, distanceTo});
+        }
+      }
+    }
+
+    return dist;
+  }
+};
 
 #endif  // ! DIJKSTRA_ALGORITHM_H_
--- Codes/data_structures/Graph/shortest_path_algorithm/dijkstra_algorithm/main.cpp
+++ Codes/data_structures/Graph/shortest_path_algorithm/dijkstra_algorithm/main.cpp
@@ -6,38 +6,38 @@
 #include "dijkstra_algorithm.h"
 
 int main(int argc, char *argv[]) {
-  std::vector<std::vector<Path>> graph(6, std::vector<Path>{});
-  graph[0].emplace_back(Path(0, 0, 0));
-  graph[0].emplace_back(Path(0, 1, 4));
-  graph[0].emplace_back(Path(0, 2, 2));
+  Dijkstra g;
+  g.addEdge(0, 0, 0);
+  g.addEdge(0, 1, 4);
+  g.addEdge(0, 2, 2);
 
-  graph[1].emplace_back(Path(1, 1, 0));
-  graph[1].emplace_back(Path(1, 0, 4));
-  graph[1].emplace_back(Path(1, 2, 1));
-  graph[1].emplace_back(Path(1, 3, 5));
+  g.addEdge(1, 1, 0);
+  g.addEdge(1, 0, 4);
+  g.addEdge(1, 2, 1);
+  g.addEdge(1, 3, 5);
 
-  graph[2].emplace_back(Path(2, 2, 0));
-  graph[2].emplace_back(Path(2, 0, 2));
-  graph[2].emplace_back(Path(2, 1, 1));
-  graph[2].emplace_back(Path(2, 3, 8));
-  graph[2].emplace_back(Path(2, 4, 9));
+  g.addEdge(2, 2, 0);
+  g.addEdge(2, 0, 2);
+  g.addEdge(2, 1, 1);
+  g.addEdge(2, 3, 8);
+  g.addEdge(2, 4, 9);
 
-  graph[3].emplace_back(Path(3, 3, 0));
-  graph[3].emplace_back(Path(3, 1, 5));
-  graph[3].emplace_back(Path(3, 2, 8));
-  graph[3].emplace_back(Path(3, 4, 2));
-  graph[3].emplace_back(Path(3, 5, 6));
+  g.addEdge(3, 3, 0);
+  g.addEdge(3, 1, 5);
+  g.addEdge(3, 2, 8);
+  g.addEdge(3, 4, 2);
+  g.addEdge(3, 5, 6);
 
-  graph[4].emplace_back(Path(4, 4, 0));
-  graph[4].emplace_back(Path(4, 3, 2));
-  graph[4].emplace_back(Path(4, 2, 9));
-  graph[4].emplace_back(Path(4, 5, 5));
+  g.addEdge(4, 4, 0);
+  g.addEdge(4, 3, 2);
+  g.addEdge(4, 2, 9);
+  g.addEdge(4, 5, 5);
 
-  graph[5].emplace_back(Path(5, 5, 0));
-  graph[5].emplace_back(Path(5, 4, 5));
-  graph[5].emplace_back(Path(5, 3, 6));
+  g.addEdge(5, 5, 0);
+  g.addEdge(5, 4, 5);
+  g.addEdge(5, 3, 6);
 
-  std::vector<int> shortestPaths = dijkstra(graph, 0);
+  std::vector<int> shortestPaths = g.dijkstra(0);
   std::cout << "dijkstra's algorithm: " << std::endl;
   for (int i = 0; i < shortestPaths.size(); ++i) {
     std::cout << "To vertex " << i << ": "
