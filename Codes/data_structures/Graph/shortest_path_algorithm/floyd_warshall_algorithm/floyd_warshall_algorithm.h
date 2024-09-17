e5ae02bb2988eda01794339d1bcd0a4a6c4d0561
Jeremy Pai <jeremypai1128@gmail.com>
2024-09-17 19:10:25 +0800

    update FloydWarshall class to includ addEdge function and improve the codes of Floyd-Warshall algorithm

--- Codes/data_structures/Graph/shortest_path_algorithm/floyd_warshall_algorithm/floyd_warshall_algorithm.h
+++ Codes/data_structures/Graph/shortest_path_algorithm/floyd_warshall_algorithm/floyd_warshall_algorithm.h
@@ -5,25 +5,52 @@
 #include <limits>
 #include <vector>
 
-inline std::vector<std::vector<int>> floydWarshall(
-    const std::vector<std::vector<int>> &graph) {
-  // Initialize distance matrix
-  std::vector<std::vector<int>> dist = graph;
+struct Edge {
+  int src;
+  int dest;
+  int weight;
+  Edge(int src, int dest, int weight) : src(src), dest(dest), weight(weight) {}
+};
 
-  for (int k = 0; k < graph.size(); ++k) {
-    for (int i = 0; i < graph.size(); ++i) {
-      for (int j = 0; j < graph.size(); ++j) {
-        // If vertex k is on the shortest path from i to j, then update the
-        // value of dist[i][j]
-        if (dist[i][k] != std::numeric_limits<int>::max() &&
-            dist[k][j] != std::numeric_limits<int>::max()) {
-          dist[i][j] = std::min(dist[i][j], dist[i][k] + dist[k][j]);
+class FloydWarshall {
+ private:
+  std::vector<Edge> edgeList_;
+  int vertexNum_ = 0;
+
+ public:
+  FloydWarshall(int v) : vertexNum_(v) {}
+  ~FloydWarshall() = default;
+
+  void addEdge(int u, int v, int w) { edgeList_.emplace_back(Edge{u, v, w}); }
+
+  std::vector<std::vector<int>> floydWarshall() {
+    // Initialize distance matrix
+    std::vector<std::vector<int>> dist(
+        vertexNum_,
+        std::vector<int>(vertexNum_, std::numeric_limits<int>::max()));
+
+    for (const auto& edge : edgeList_) {
+      const auto& src = edge.src;
+      const auto& dest = edge.dest;
+      const auto& weight = edge.weight;
+      dist[src][dest] = weight;
+    }
+
+    for (int k = 0; k < vertexNum_; ++k) {
+      for (int i = 0; i < vertexNum_; ++i) {
+        for (int j = 0; j < vertexNum_; ++j) {
+          // If vertex k is on the shortest path from i to j, then update the
+          // value of dist[i][j]
+          if (dist[i][k] != std::numeric_limits<int>::max() &&
+              dist[k][j] != std::numeric_limits<int>::max()) {
+            dist[i][j] = std::min(dist[i][j], dist[i][k] + dist[k][j]);
+          }
         }
       }
     }
-  }
 
-  return dist;
-}
+    return dist;
+  }
+};
 
 #endif  // ! FLOYD_WARSHALL_ALGORITHM_H_
--- Codes/data_structures/Graph/shortest_path_algorithm/floyd_warshall_algorithm/main.cpp
+++ Codes/data_structures/Graph/shortest_path_algorithm/floyd_warshall_algorithm/main.cpp
@@ -44,19 +44,53 @@ int main(int argc, char *argv[]) {
       std::cout << (graph[i][j] == std::numeric_limits<int>::max()
                         ? "INF"
                         : std::to_string(graph[i][j]))
-                << " ";
+                << "\t";
     }
     std::cout << std::endl;
   }
 
-  std::vector<std::vector<int>> shortestPaths = floydWarshall(graph);
+  // Floyd-Warshall algorithm
+  int vertexNum = 6;
+  FloydWarshall g(vertexNum);
+
+  g.addEdge(0, 0, 0);
+  g.addEdge(0, 1, 4);
+  g.addEdge(0, 2, 2);
+
+  g.addEdge(1, 1, 0);
+  g.addEdge(1, 0, 4);
+  g.addEdge(1, 2, 1);
+  g.addEdge(1, 3, 5);
+
+  g.addEdge(2, 2, 0);
+  g.addEdge(2, 0, 2);
+  g.addEdge(2, 1, 1);
+  g.addEdge(2, 3, 8);
+  g.addEdge(2, 4, 9);
+
+  g.addEdge(3, 3, 0);
+  g.addEdge(3, 1, 5);
+  g.addEdge(3, 2, 8);
+  g.addEdge(3, 4, 2);
+  g.addEdge(3, 5, 6);
+
+  g.addEdge(4, 4, 0);
+  g.addEdge(4, 3, 2);
+  g.addEdge(4, 2, 9);
+  g.addEdge(4, 5, 5);
+
+  g.addEdge(5, 5, 0);
+  g.addEdge(5, 4, 5);
+  g.addEdge(5, 3, 6);
+
+  std::vector<std::vector<int>> shortestPaths = g.floydWarshall();
   std::cout << "floyd-warshall algorithm: " << std::endl;
   for (int i = 0; i < shortestPaths.size(); ++i) {
     for (int j = 0; j < shortestPaths.size(); ++j) {
       std::cout << (shortestPaths[i][j] == std::numeric_limits<int>::max()
                         ? "INF"
                         : std::to_string(shortestPaths[i][j]))
-                << " ";
+                << "\t";
     }
     std::cout << std::endl;
   }
